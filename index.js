49e121e2c6a077c99770ac5007f1fb4aa331d703
Morten Nielsen <morten@morkeleb.com>
2020-11-03 22:06:28 +0100

    adjusting formatter to promises

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const prettier = require('prettier');
 module.exports = {
     extensions: ['**/*.js', '**/*.jsx'],
-    apply: function(content) {
-        return prettier.format(content);
+    apply: async function(file_path, content) {
+        const options = await  prettier.resolveConfig(file_path);
+        return prettier.format(content, {filepath: file_path, ...options});
     }
 }
