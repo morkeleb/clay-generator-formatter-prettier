48baf4f22ca507fee227992d259bb1dd7460befe
Morten Nielsen <morten@morkeleb.com>
2021-11-26 10:38:28 +0100

    the formatter now organizes imports as well

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const prettier = require('prettier');
 module.exports = {
-    extensions: ['**/*.js', '**/*.jsx'],
+    extensions: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
     apply: async function(file_path, content) {
         const options = await  prettier.resolveConfig(file_path);
         return prettier.format(content, {filepath: file_path, ...options});
